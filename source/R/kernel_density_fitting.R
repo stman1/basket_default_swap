8184659ab982e1c9f57ae6cc382b7492602a07bb
Stefan Mangold <stefanmangold1@gmail.com>
2022-07-28 09:59:29 +0200

    R code WIP

--- source/R/kernel_density_fitting.R
+++ source/R/kernel_density_fitting.R
@@ -1,21 +1,34 @@
 # Load cds spreads from Excel sheet
 
-library(readxl) # Excel readerb package
+library(readxl) # Excel reader package
 library(dplyr) # Dataframe manipulation package
 library(zoo) # Dataframe manipulation package
 
-
-
+# read in Excel file, tab CDS_spreads_history into R dataframe
 cds_basket_data=read_excel('Documents/CQF/basket_default_swap/data/default_basket_data.xlsx', 
                            'CDS_spreads_history',
                            range = 'E21:N1853')
-cds_basket_data # show cds dataframe
 
-plot(cds_basket_data$`Banco Santander`, type='o', pch=10, col='steelblue', xlab='Time Stamp', ylab='Price')
+# show cds dataframe
+cds_basket_data 
+
+# plot with gaps
+plot(cds_basket_data$`Level Banco Santander`, type='o', pch=10, col='steelblue', xlab='Time Stamp', ylab='Price')
 
 #interpolate missing values in 'Banco Santander' column
 cds_basket_data <- cds_basket_data %>%
-  mutate(`Banco Santander` = na.approx(`Banco Santander`))
+  mutate(`Level Banco Santander` = na.approx(`Level Banco Santander`))
+
+# plot without gaps
+plot(cds_basket_data$`Level Banco Santander`, type='o', pch=10, col='chocolate4', xlab='Time Stamp', ylab='Price')
+
+# compute returns
+cds_basket_data %>%
+  mutate(across(everything(), .funs = funs((~(lead(.) - .) / .))))
+
+
+# cds_basket_data %>%
+# mutate(across('Level Credit Suisse'), .funs = (DR = list(~(lead(.) - .) / .)))
 
 
 pseudo.uniform = function(X){
@@ -24,4 +37,4 @@ pseudo.uniform = function(X){
 }
 
 
-num_missing_values <-sum(is.na(cds_basket_data$'Credit Suisse'))
+num_missing_values <-sum(is.na(cds_basket_data$'Level Credit Suisse'))
