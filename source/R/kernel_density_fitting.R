7b1a47d70c6a14e90aec6c8194611075b28425cf
Stefan Mangold <stefanmangold1@gmail.com>
2022-07-30 20:39:38 +0200

    WIP 30.07.2022

--- source/R/kernel_density_fitting.R
+++ source/R/kernel_density_fitting.R
@@ -3,33 +3,45 @@
 library(readxl) # Excel reader package
 library(dplyr) # Dataframe manipulation package
 library(zoo) # Dataframe manipulation package
+library(ks) # kernel smoothing package, alternative to MATLAB ksdensity
+
 
 # read in Excel file, tab CDS_spreads_history into R dataframe
-cds_basket_data=read_excel('Documents/CQF/basket_default_swap/data/default_basket_data.xlsx', 
+cds_spread_data=read_excel('C:/CQF/Project/data/default_basket_data.xlsx', 
                            'CDS_spreads_history',
                            range = 'E21:N1853')
 
+# count missing values
+missing_values <- cds_spread_data %>% select(is.numeric) %>%
+  summarise_all(list(~(sum(is.na(.)))))
+
+# Drop Banco Santander, Standard Chartered, Danske Bank
+cds_spread_data <- subset(cds_spread_data, select = -c(`Banco Santander`, `Standard Chartered`, `Danske Bank`))
+
+#interpolate missing values for all columns
+#cds_spread_data <- cds_spread_data %>%
+#  mutate(`Level Banco Santander` = na.approx(`Level Banco Santander`))
+
+cds_spread_data <- cds_spread_data %>%
+  mutate(across(is.numeric, list(~na.approx(.))))
+
 # show cds dataframe
-cds_basket_data 
+#cds_basket_data 
 
 # plot with gaps
-plot(cds_basket_data$`Level Banco Santander`, type='o', pch=10, col='steelblue', xlab='Time Stamp', ylab='Price')
-
-#interpolate missing values in 'Banco Santander' column
-cds_basket_data <- cds_basket_data %>%
-  mutate(`Level Banco Santander` = na.approx(`Level Banco Santander`))
+#plot(cds_spread_data$`Level Banco Santander`, type='o', pch=10, col='steelblue', xlab='Time Stamp', ylab='Price')
 
 # plot without gaps
-plot(cds_basket_data$`Level Banco Santander`, type='o', pch=10, col='chocolate4', xlab='Time Stamp', ylab='Price')
+#plot(cds_spread_data$`Level Banco Santander`, type='o', pch=10, col='chocolate4', xlab='Time Stamp', ylab='Price')
 
 # compute returns
-cds_basket_data %>%
-  mutate(across(everything(), .funs = funs((~(lead(.) - .) / .))))
 
+cds_spread_data <- cds_spread_data %>%
+  mutate(across(is.numeric, list(perc_change = ~(./lag(.) - 1))))
 
-# cds_basket_data %>%
-# mutate(across('Level Credit Suisse'), .funs = (DR = list(~(lead(.) - .) / .)))
+cds_spread_data
 
+num_missing_values <-sum(is.na(cds_spread_data$'Level Credit Suisse'))
 
 pseudo.uniform = function(X){
   Fhat <- kcde(X)
@@ -37,4 +49,8 @@ pseudo.uniform = function(X){
 }
 
 
-num_missing_values <-sum(is.na(cds_basket_data$'Level Credit Suisse'))
+
+
+
+
+
