912d711de1bfd1ad92297cc96e6e2be8efd6c59a
Stefan Mangold <stefanmangold1@gmail.com>
2022-08-01 13:24:21 +0200

    WIP R code 01.08.2022

--- source/R/kernel_density_fitting.R
+++ source/R/kernel_density_fitting.R
@@ -3,11 +3,14 @@
 library(readxl) # Excel reader package
 library(dplyr) # Dataframe manipulation package
 library(zoo) # Dataframe manipulation package
-library(ks) # kernel smoothing package, alternative to MATLAB ksdensity
+library(ks) # kernel smoothing package, alternative to MATLAB ksdensity function
+library(psych) # to plot nice scatterplot matrices
 
+# set path to data
 
+data.path <- file.path(here(), 'data')
 # read in Excel file, tab CDS_spreads_history into R dataframe
-cds_spread_data=read_excel('C:/CQF/Project/data/default_basket_data.xlsx', 
+cds_spread_data=read_excel(file.path(data.path, 'default_basket_data.xlsx'),
                            'CDS_spreads_history',
                            range = 'E21:N1853')
 
@@ -19,14 +22,11 @@ missing_values <- cds_spread_data %>% select(is.numeric) %>%
 cds_spread_data <- subset(cds_spread_data, select = -c(`Banco Santander`, `Standard Chartered`, `Danske Bank`))
 
 #interpolate missing values for all columns
-#cds_spread_data <- cds_spread_data %>%
-#  mutate(`Level Banco Santander` = na.approx(`Level Banco Santander`))
-
 cds_spread_data <- cds_spread_data %>%
-  mutate(across(is.numeric, list(~na.approx(.))))
+  mutate_at(.vars = c('Credit Suisse', 'BNP', 'Deutsche Bank', 'Societe Generale', 'HSBC', 'UBS'), list(~na.approx(.)))
 
 # show cds dataframe
-#cds_basket_data 
+#cds_spread_data 
 
 # plot with gaps
 #plot(cds_spread_data$`Level Banco Santander`, type='o', pch=10, col='steelblue', xlab='Time Stamp', ylab='Price')
@@ -35,14 +35,34 @@ cds_spread_data <- cds_spread_data %>%
 #plot(cds_spread_data$`Level Banco Santander`, type='o', pch=10, col='chocolate4', xlab='Time Stamp', ylab='Price')
 
 # compute returns
-
 cds_spread_data <- cds_spread_data %>%
-  mutate(across(is.numeric, list(perc_change = ~(./lag(.) - 1))))
+  mutate(across(is.numeric, list(ret = ~(./lag(.) - 1))))
 
 cds_spread_data
 
-num_missing_values <-sum(is.na(cds_spread_data$'Level Credit Suisse'))
 
+# scatter plot
+
+pairs.panels(cds_spread_data[,2:6], 
+             method = "pearson", # correlation method
+             hist.col = "#00AFBB",
+             density = TRUE,  # show density plots
+             ellipses = TRUE # show correlation ellipses
+)
+
+
+# compute empirical CDF of the CDS spread return series
+
+fhat_credit_suisse <- pseudo.uniform(cds_spread_data$`Credit Suisse`)
+
+
+plot(fhat_credit_suisse, ylab="Distribution function", add=FALSE, drawpoints=FALSE,
+     col.pt=2, jitter=FALSE, alpha=1)
+
+histde(fhat_credit_suisse)
+plot(histde(fhat_credit_suisse))
+
+# Functions
 pseudo.uniform = function(X){
   Fhat <- kcde(X)
   predict(Fhat, x=X)
